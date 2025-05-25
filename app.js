ed239bd4018bfe3c27d0ffe8ea1a22110f55a61e
Kilari Divya <divyakilari897@gmail.com>
2025-05-25 18:41:59 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,34 @@
+// Load the IFrame Player API code asynchronously.
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// Create a YouTube player after the API code downloads.
+var player;
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+        height: '390',
+        width: '640',
+        videoId: 'NGEyIfX4GGk',
+        events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+        }
+    });
+}
+
+// The API will call this function when the video player is ready.
+function onPlayerReady(event) {
+    event.target.playVideo();
+}
+
+// The API calls this function when the player's state changes.
+function onPlayerStateChange(event) {
+    // You can add custom behavior here if needed
+}
+
+// Stop the video playback
+function stopVideo() {
+    player.stopVideo();
+}
